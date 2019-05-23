9dc77fde23f4d0c5acca67cc1fd683eb9eac9ba5
vftens <aico@ya.ru>
2019-05-23 11:24:36 +0300

    New changes on 23 may 2019

--- greedy_player_gtp.py
+++ greedy_player_gtp.py
@@ -2,10 +2,10 @@ from AlphaGo.ai import GreedyPolicyPlayer
 from AlphaGo.models.policy import CNNPolicy
 from interface.gtp_wrapper import run_gtp
 
-MODEL = r'RocAlphaGo.data\models\CNNPolicy\48plane_192filter_12layer_keras2.json'
+MODEL = r'..\RocAlphaGo.data\models\CNNPolicy\48plane_192filter_12layer_keras2.json'
 
 #'tests/test_data/minimodel_policy.json'
-WEIGHTS = r'RocAlphaGo.data\training_results\supervised\48plane_KGS_6d_higher\accuracy_57_filters_192\weights.00120.hdf5'
+WEIGHTS = r'..\RocAlphaGo.data\training_results\supervised\48plane_KGS_6d_higher\accuracy_57_filters_192\weights.00116.hdf5'
 #'tests/test_data/traindata/weights.00999.hdf5'
 
 policy = CNNPolicy.load_model(MODEL)
